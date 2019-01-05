a76a49732ffa1afda028053927995b7ef7a7f540
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-05 22:11:43 +0530

    Setup Provider, store and middleware

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import middleware from './middleware'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, middleware)
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
