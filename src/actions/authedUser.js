087bda41de53383267c28c6786f30c55cff0233b
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-06 16:34:23 +0530

    Added action and reducer for removing authedUser

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,15 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
+export const REMOVE_AUTHED_USER = 'REMOVE_AUTHED_USER'
 
 export const setAuthedUser = (id) => {
   return {
     type: SET_AUTHED_USER,
     id
+  }
+}
+
+export const removeAuthedUser = () => {
+  return {
+    type: REMOVE_AUTHED_USER
   }
 }
