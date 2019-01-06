a157e43c80424855099d0d38a7eb83404415edfc
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-06 15:30:50 +0530

    Update Navbar add signout and present user info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { removeAuthedUser } from '../actions/authedUser'
 
 class NavBar extends React.Component {
 
+  signOut = () => {
+    this.props.dispatch(removeAuthedUser())
+  }
+
   render() {
+    const { authedUser, user } = this.props
+
     return (
       <nav>
         <ul>
@@ -12,13 +20,32 @@ class NavBar extends React.Component {
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink to='/signin'>Sign In</NavLink>
-          </li>
+          {authedUser
+            ?
+            <span>
+              <p>Hello, {user ? user.name : null}</p>
+              <li>
+                <button onClick={this.signOut}>Sign Out</button>
+              </li>
+            </span>
+            :
+            <li>
+              <NavLink to='/signin'>Sign In</NavLink>
+            </li>
+          }
         </ul>
       </nav>
     )
   }
 }
 
-export default NavBar
+const mapStateToProps = ({ authedUser, users }) => {
+  const user = users[authedUser]
+
+  return {
+    authedUser,
+    user
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
