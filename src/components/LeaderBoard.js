416fe55dc33f35fc2eac869f954b2e63928aacb7
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-06 16:32:31 +0530

    Scorewise list displayed on leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,17 +3,32 @@ import { connect } from 'react-redux'
 
 class LeaderBoard extends React.Component {
   render() {
+    const { positions } = this.props
+
     return (
       <div>
-        LeaderBoard
+        {positions.map((position) => 
+            <div>
+              {position.id} - {position.score}
+            </div>
+        )}
       </div>
     )
   }
 }
 
 const mapStateToProps = ({ users }) => {
+  const userIds = Object.keys(users)
+  const positions = userIds.map((id) => ({
+    id,
+    score: 
+      Object.keys(users[id].answers).length +
+      Object.keys(users[id].questions).length 
+  }))
+
   return {
-    users
+    users,
+    positions: positions.sort((a, b) => b.score - a.score)
   }
 }
 
