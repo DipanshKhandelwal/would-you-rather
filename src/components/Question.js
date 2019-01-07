a8d759c392e7ebd3bfcc341b087760d7cff9771b
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-08 02:48:04 +0530

    Update Question for displying results, form and handle answering

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,54 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { handleAnswerQuestion } from '../actions/questions'
+import PollResults from './PollResults'
+import PollForm from './PollForm'
 
 class Question extends React.Component {
+
+  submitAnswer = ({ answer }) => {
+    const { dispatch, id } = this.props
+    dispatch(handleAnswerQuestion({ qid: id, answer }))
+  }
+
   render() {
+    const { users, authedUser, questions, id } = this.props
+    const status = Object.keys(users[authedUser].answers).includes(id) ? 'answered' : 'unanswered'
+    const optionOneVotes = questions[id].optionOne.votes.length
+    const optionOneText = questions[id].optionOne.text
+    const optionTwoVotes = questions[id].optionTwo.votes.length
+    const optionTwoText = questions[id].optionTwo.text
+
     return (
       <div>
-        Question - {this.props.id}
+        <p>Question - {id}</p>
+        <p>{status}</p>
+        {status === 'answered'
+          ? <PollResults
+            optionOneVotes={optionOneVotes}
+            optionTwoVotes={optionTwoVotes}
+            optionOneText={optionOneText}
+            optionTwoText={optionTwoText}
+          />
+          : <PollForm
+            optionOneText={optionOneText}
+            optionTwoText={optionTwoText}
+            submitAnswer={this.submitAnswer}
+          />
+        }
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ questions }, props) => {
+const mapStateToProps = ({ questions, users, authedUser }, props) => {
   const { id } = props.match.params
   return {
-    id
+    id,
+    questions,
+    users,
+    authedUser
   }
 }
 
