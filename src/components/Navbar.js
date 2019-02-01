d35dea0ad9890f7deace00876f310f5b706f79cd
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-02-01 23:11:19 +0530

    Added material UI and update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import FiberManualRecord from '@material-ui/icons/FiberManualRecord'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { removeAuthedUser } from '../actions/authedUser'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class NavBar extends React.Component {
 
@@ -10,52 +30,61 @@ class NavBar extends React.Component {
   }
 
   render() {
-    const { authedUser, user } = this.props
-
+    const { classes, authedUser, user } = this.props;
     return (
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='/home' activeStyle={{ fontWeight: 'bold' }} >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/newquestion' activeStyle={{ fontWeight: 'bold' }} >
-              New Question
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/leaderboard' activeStyle={{ fontWeight: 'bold' }} >
-              Leaderboard
-            </NavLink>
-          </li>
-          {authedUser
-            ?
-            <span>
-              <p>Hello, {user ? user.name : null}</p>
-              <li>
-                <button onClick={this.signOut}>Sign Out</button>
-              </li>
-            </span>
-            :
-            <li>
-              <NavLink to='/signin' activeStyle={{ fontWeight: 'bold' }}  >Sign In</NavLink>
-            </li>
-          }
-        </ul>
-      </nav>
-    )
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              <Link to='/' style={{ color: 'white', textDecoration: 'none' }} >
+                Would You Rather ?
+            </Link>
+            </Typography>
+            <Link to='/home' style={{ color: 'white', textDecoration: 'none' }} >
+              <Button color="inherit">Home</Button>
+            </Link>
+            <Link to='/newquestion' style={{ color: 'white', textDecoration: 'none' }} >
+              <Button color="inherit">New Question</Button>
+            </Link>
+            <Link to='/leaderboard' style={{ color: 'white', textDecoration: 'none' }} >
+              <Button color="inherit">Leaderboard</Button>
+            </Link>
+            {authedUser
+              ?
+              <span
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginLeft: 20,
+                }} >
+                <FiberManualRecord style={{ color: 'green', fontSize: 20 }} />
+                <p style={{ marginRight: 20 }} >{user ? user.name : null}</p>
+                <Button onClick={this.signOut} color="inherit">Logout</Button>
+              </span>
+              :
+              <Link to='/signin' style={{ color: 'white', textDecoration: 'none' }} >
+                <Button color="inherit">Login</Button>
+              </Link>
+            }
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
   }
 }
 
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 const mapStateToProps = ({ authedUser, users }) => {
   const user = users[authedUser]
-
   return {
     authedUser,
     user
   }
 }
 
-export default connect(mapStateToProps)(NavBar)
+export default connect(mapStateToProps)(withStyles(styles)(NavBar))
