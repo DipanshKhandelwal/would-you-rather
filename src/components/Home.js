3de84e83b305c2b83f94e5412ddd997072f11ba5
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-02-04 10:21:33 +0530

    Divided home to answered and unanswered questions and added cardview

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,72 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Card from '@material-ui/core/Card'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
-  render() {
 
+  state = {
+    tab: 0
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ tab: value });
+  }
+
+  render() {
     const { questionsIds, users, authedUser, questions } = this.props
+    const { tab } = this.state
 
     return (
-      <ul>{
-        questionsIds.map((id) => {
-          return (
-            <li key={id}>
-              <h2>Question by {users[questions[id].author].name}</h2>
-              <Link to={`/question/${id}`} >{id}</Link>
-              - {Object.keys(users[authedUser].answers).includes(id) ? 'answered' : 'unanswered'}
-              - <Link to={`/question/${id}`} >View poll</Link>
-            </li>
-          )
-        })
-      }</ul>
+      <div style={{ padding: 15, display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+        <Card style={{ display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+          <AppBar position="static" style={{ display: 'flex', flex: 1, justifyContent: 'center' }} >
+            <Tabs value={tab} onChange={this.handleChange} style={{ display: 'flex', flex: 1, justifyContent: 'center' }} >
+              <Tab label="Answered" />
+              <Tab label="Unanswered" />
+            </Tabs>
+          </AppBar>
+          {tab === 0 &&
+            <ul style={{ padding: 0 }} >{
+              questionsIds.map((id) => {
+                if (Object.keys(users[authedUser].answers).includes(id)) {
+                  return (
+                    <Card key={id} style={{ padding: 10, margin: 10, display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                      <h2>{users[questions[id].author].name} asks ... </h2>
+                      ... {questions[id].optionOne.text} ... Or
+                      <Link to={`/question/${id}`} style={{ textDecoration: 'none' }} >
+                        <Button style={{ margin: 10, display: 'flex', flex: 1 }} variant="contained" color="secondary" >View Poll</Button>
+                      </Link>
+                    </Card>
+                  )
+                }
+              })
+            }</ul>
+          }
+          {tab === 1 &&
+            <ul style={{ padding: 0 }} >{
+              questionsIds.map((id) => {
+                if (!Object.keys(users[authedUser].answers).includes(id)) {
+                  return (
+                    <Card key={id} style={{ padding: 10, margin: 10, display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                      <h2>{users[questions[id].author].name} asks ... </h2>
+                      ... {questions[id].optionOne.text} ... Or
+                      <Link to={`/question/${id}`} style={{ textDecoration: 'none' }} >
+                        <Button style={{ margin: 10, display: 'flex', flex: 1 }} variant="contained" color="secondary" >View Poll</Button>
+                      </Link>
+                    </Card>
+                  )
+                }
+              })
+            }</ul>
+          }
+
+        </Card>
+      </div>
     )
   }
 }
