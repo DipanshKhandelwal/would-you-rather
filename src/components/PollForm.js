e4f1677cdc751cbca94766e234e4014936a4255a
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-02-04 10:22:12 +0530

    PollForm UI update

--- src/components/PollForm.js
+++ src/components/PollForm.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
+import Button from '@material-ui/core/Button'
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormLabel from '@material-ui/core/FormLabel'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
 
 class PollForm extends React.Component {
 
@@ -6,36 +13,38 @@ class PollForm extends React.Component {
     selected: 'optionOne'
   }
 
-  handleOptionChange = (e) => {
-    this.setState({
-      selected: e.target.value
-    })
-  }
+  handleChange = event => {
+    this.setState({ selected: event.target.value });
+  };
+
 
   render() {
     const { optionOneText, optionTwoText, submitAnswer } = this.props
     const { selected } = this.state
 
     return (
-      <div>
-        <h3>Would you rather ...</h3>
-        <label>
-          <input type="radio"
-            value="optionOne"
-            checked={selected === 'optionOne'}
-            onChange={this.handleOptionChange}
-          />
-          {optionOneText}
-        </label>
-        <label>
-          <input type="radio"
-            value="optionTwo"
-            checked={selected === 'optionTwo'}
-            onChange={this.handleOptionChange}
-          />
-          {optionTwoText}
-        </label>
-        <button onClick={() => submitAnswer({answer: selected})} >Submit</button>
+      <div style={{ padding: 15, display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+        <Card style={{ padding: 50, display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+          <h3>Would you rather ...</h3>
+          <FormControl component="fieldset" >
+            <RadioGroup
+              aria-label="Gender"
+              name="gender1"
+              value={this.state.selected}
+              onChange={this.handleChange}
+            >
+              <FormControlLabel value="optionOne" control={<Radio />} label={optionOneText} />
+              <FormControlLabel value="optionTwo" control={<Radio />} label={optionTwoText} />
+            </RadioGroup>
+          </FormControl>
+          <Button
+            style={{ margin: 10, display: 'flex', flex: 1 }}
+            variant="contained"
+            color="secondary"
+            onClick={() => submitAnswer({ answer: selected })}>
+            Submit
+          </Button>
+        </Card>
       </div>
     )
   }
